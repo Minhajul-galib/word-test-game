d8806fb9feef40d98d1d77374b4c1bdb05c964ed
Minhajul Galib <62963105+Minhajul-galib@users.noreply.github.com>
2021-09-17 21:45:31 +0600

    3rd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let mainTextShowId = document.getElementById('word-show');
 let resultText = document.getElementById('Text-show');
 let typedShow = document.getElementById('type-word');
+let scoreId = document.getElementById('score');
+let nameTextId = document.getElementById('name-text');
+let ok;
 
 let words =[
 'abandon',
@@ -31,8 +34,15 @@ let words =[
 'abbreviation',
 'abbreviatory'
 ];
+let playerName;
+function nameBtn(){
+    let nameInput = document.getElementById('name').value;
+    document.getElementById('name-text').innerHTML = nameInput;
+    return nameInput;
+// let playerName = document.getElementById('name-text').textContent;
 
-console.log(words);
+}
+
 function randomWord(){
     const index = Math.round(Math.random() * 27);
     const takeWord = words[index];
@@ -40,11 +50,13 @@ function randomWord(){
 }
 let ranWord = randomWord();
 function start(){
+    typedShow.style.display = 'none';
     mainTextShowId.innerText = `${ranWord}`;
     sessionStorage.setItem('got-word', ranWord);
     setTimeout(() => {
         mainTextShowId.innerText = ''.padEnd(ranWord.length * 1, '-');
     }, 3000);
+    document.getElementById('start').disabled = true;
 }
 
 const character = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -75,33 +87,39 @@ document.querySelector('.btn-div').addEventListener('click', function(e){
             
             
     });
-
 function submitBtn(){
+    
     document.getElementById('result-div').style.display = 'block';
     const testCorrect = typedShow.textContent;
     const sessionWord = sessionStorage.getItem('got-word');
     console.log(sessionWord);
     if(testCorrect === sessionWord){
-        resultText.innerText = `Congratulations🥳🥳 You got 5marks`;
+        resultText.innerText = `Congratulations🥳🥳 You got 5 marks`;
+        
+        const playerScore = 5;
+        let preScore = scoreId.innerText;
+        if(preScore === ''){
+            preInt = 0;
+        }
+        else{
+            preInt = parseInt(preScore)
+        }
+
+        const totalScore = preInt + playerScore; 
         resultText.style.color = '#48C9B0'
+        localStorage.setItem(nameBtn(), totalScore);
+        ok = nameBtn();
+        scoreId.innerText = localStorage.getItem(ok);
+        setInterval(() => {
+        location.reload();
+        }, 2000);
         }
     else{
         resultText.innerText = `Opps🥵🥵 your word is not correct`;
         resultText.style.color = '#48C9B0'
+        setInterval(() => {
+            location.reload();
+            }, 2000);
         }
 }
-
-// document.getElementById('submit-btn').addEventListener('click', function(){
-    
-// });
-// use
-// const clickedBtn = event.target.innerText;
-
-// document.querySelector('.btn-div').addEventListener('click', function(event){
-//     
-//     if(clickedBtn === 'Submit'){
-
-//         
-//     }
-// });
-
+scoreId.innerText = localStorage.getItem(ok);
