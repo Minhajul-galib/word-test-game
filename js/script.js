d86a878dfdc98350001b05baea5bc79af8acdaea
Minhajul Galib <62963105+Minhajul-galib@users.noreply.github.com>
2021-09-24 14:24:01 +0600

    10th

--- js/script.js
+++ js/script.js
@@ -3,60 +3,54 @@ let resultText = document.getElementById('Text-show');
 let typedShow = document.getElementById('type-word');
 let scoreId = document.getElementById('score');
 let nameTextId = document.getElementById('name-text');
-let ok;
-
+let inputName = document.getElementById('name');
+let startBtn = document.getElementById('start');
+let submitButton = document.getElementById('sub-btn');
+let name = inputName.value
+let makeWord;
 let words =[
 'abandon',
 'desert',
 'desert',
-'abatis',
-'abattage',
-'abattoir',
-'abaxial',
-'abb',
-'abba',
-'abbacy',
-'Abbai',
-'Abbasid',
-'abbatial',
-'abbess',
-'Abbeville',
-'Abbevillian',
-'abbey',
-'Abbie',
-'abbot',
-'Abbotsford',
-'Abbott',
-'Abboud',
-'abbr',
-'abbreviate',
-'abbreviated',
-'abbreviation',
-'abbreviatory'
+'abatis'
 ];
 let playerName;
-function nameBtn(){
-    let nameInput = document.getElementById('name').value;
-    document.getElementById('name-text').innerHTML = nameInput;
-    return nameInput;
-// let playerName = document.getElementById('name-text').textContent;
 
+// Add Your NAme Click Modal show!
+document.querySelector('#name-adder').addEventListener('click', function(){
+    document.querySelector('.main-modal').style.display = 'block';
+});
+
+
+
+function nameBtn(){
+
+    nameTextId.innerHTML = inputName.value;
+    document.querySelector('.main-modal').style.display = 'none';
+    
+    const name = inputName.value;
+    const nameScore = localStorage.getItem(name);
+    scoreId.innerText = nameScore;
 }
 
 function randomWord(){
-    const index = Math.round(Math.random() * 27);
+    const index = Math.round(Math.random() * 4);
     const takeWord = words[index];
     return takeWord.toUpperCase();
 }
-let ranWord = randomWord();
+let ranWord;
+
 function start(){
+    ranWord = randomWord();
     typedShow.style.display = 'none';
+    typedShow.innerText = '';
     mainTextShowId.innerText = `${ranWord}`;
     sessionStorage.setItem('got-word', ranWord);
     setTimeout(() => {
         mainTextShowId.innerText = ''.padEnd(ranWord.length * 1, '-');
     }, 3000);
     document.getElementById('start').disabled = true;
+    document.querySelector('.sub-button').disabled = false;
 }
 
 const character = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -81,14 +75,22 @@ document.querySelector('.btn-div').addEventListener('click', function(e){
             mainTextShowId.innerText ='';
             typedShow.style.display = 'block'
             const preValue = typedShow.innerText;
-            const makeWord = preValue + clickedBtn;
+            makeWord = preValue + clickedBtn;
             typedShow.innerText = makeWord;
         }
             
             
     });
+
+    // delete typed word!!
+    function deleteBtn(){
+        removecharecter = makeWord.slice(0, makeWord.length - 1);
+        makeWord = removecharecter;
+        typedShow.innerText = makeWord;
+    }
+
 function submitBtn(){
-    
+ 
     document.getElementById('result-div').style.display = 'block';
     const testCorrect = typedShow.textContent;
     const sessionWord = sessionStorage.getItem('got-word');
@@ -97,6 +99,7 @@ function submitBtn(){
         resultText.innerText = `Congratulations🥳🥳 You got 5 marks`;
         
         const playerScore = 5;
+
         let preScore = scoreId.innerText;
         if(preScore === ''){
             preInt = 0;
@@ -107,19 +110,35 @@ function submitBtn(){
 
         const totalScore = preInt + playerScore; 
         resultText.style.color = '#48C9B0'
-        localStorage.setItem(nameBtn(), totalScore);
-        ok = nameBtn();
-        scoreId.innerText = localStorage.getItem(ok);
-        setInterval(() => {
-        location.reload();
-        }, 2000);
+
+       // Add score text!
+       scoreId.innerText = totalScore;
+
+       // name set!
+       const playerName = nameTextId.textContent; 
+        
+        localStorage.setItem(playerName, totalScore);
+
+        // setInterval(() => {
+        // location.reload();
+        // }, 2000);
         }
     else{
         resultText.innerText = `Opps🥵🥵 your word is not correct`;
         resultText.style.color = '#48C9B0'
-        setInterval(() => {
-            location.reload();
-            }, 2000);
         }
+
+    document.getElementById('start').disabled = false;
+    document.querySelector('.sub-button').disabled = true;
+    
 }
-scoreId.innerText = localStorage.getItem(ok);
+scoreId.innerText = localStorage.getItem(playerName);
+
+
+function newWordAdd(){
+    // new word add input!
+    const newWord = document.querySelector('#word-input').value;
+    words.push(newWord);
+    console.log(words);
+}
+
